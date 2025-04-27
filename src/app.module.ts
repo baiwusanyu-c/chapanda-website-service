6392c7022ccc697c336cc4dfa51efd465e7131da
baiwusanyu-c <740132583@qq.com>
2025-04-27 10:49:44 +0800

    chore: 环境变量集成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
+
+const ENV_PATH = path.join(process.cwd(), `./env/.env.${process.env.APP_ENV}`);
+console.log(ENV_PATH);
 
 @Module({
-  imports: [],
+  imports: [
+    // 配置环境变量
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [ENV_PATH],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
