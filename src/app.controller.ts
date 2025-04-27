5905ae1b9c886f572ac037d54edb853d794c08b4
baiwusanyu-c <740132583@qq.com>
2025-04-27 11:24:41 +0800

    chore: swagger、apifox集成

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // 定义接口描述
+  @ApiOperation({ summary: '密码登录', description: '密码登录接口' })
+  // 定义接口返回
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'aaa 成功',
+    type: String,
+  })
+  // 定义参数
+  // @ApiBody({
+  //   type: LoginUserDto,
+  // })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app_port') || 3000;
+
+  // 集成 swagger，配置 builder
+  const config = new DocumentBuilder()
+    .setTitle('chapanda-website-service') // 项目名称 title
+    .setDescription('chapanda website 后端接口文档') // 项目描述
+    .setVersion('0.0.0') // 项目 api 版本
+    .addTag('当前环境：' + process.env.APP_ENV!) // 项目 api 标签分类
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // 启动 swagger。 docs 是路由，同时也是 api json 前缀(localhost:8084/docs-json)
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
 }
 bootstrap();
