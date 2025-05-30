6d2fde2f093462d62b5b57eb0044d2203229a10f
baiwusanyu-c <740132583@qq.com>
2025-05-30 11:27:35 +0800

    chore: temp commit40

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,6 @@ docker-compose build;
 # 启动并后台运行
 docker-compose up -d;
 # 查看日志
-docker logs chapanda-website-service;
+docker logs chapanda-nginx-container;
 # 对空间进行自动清理
 docker system prune -a -f
